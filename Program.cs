7262706c8432132c508fc97a79e7452178f29fe7
Vladimir Berezovskiy <arctick777@rambler.ru>
2024-07-17 18:50:24 +0700

    Added a solution for task2

--- Program.cs
+++ Program.cs
@@ -2,18 +2,33 @@
 // которая выведет все натуральные числа в промежутке от M до N.
 // Использовать рекурсию, не использовать циклы.
 
-void PrintMToN(int M, int N)
+// void PrintMToN(int M, int N)
+// {
+//     if (M <= N - 2)
+//     {
+//         Console.Write(M + 1 + " ");
+//         PrintMToN(M + 1, N);
+//     }
+// }
+
+// Console.WriteLine("Введите начальное число: ");
+// int A = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine($"Введите второе начальное число, которое будет больше {A}: ");
+// int B = Convert.ToInt32(Console.ReadLine());
+
+// PrintMToN(A, B);
+
+
+//----------------------------------------------------------------
+//Задача 2: Напишите программу вычисления функции Аккермана
+//с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+int Akkerman(int m, int n)
 {
-    if (M <= N - 2)
-    {
-        Console.Write(M + 1 + " ");
-        PrintMToN(M + 1, N);
-    }
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+
 }
 
-Console.WriteLine("Введите начальное число: ");
-int A = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Введите второе начальное число, которое будет больше {A}: ");
-int B = Convert.ToInt32(Console.ReadLine());
-
-PrintMToN(A, B);
+Console.WriteLine(Akkerman(3, 11));
